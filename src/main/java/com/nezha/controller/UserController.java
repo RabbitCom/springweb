c553bc5f7e3f201edef274b10763cb45f9391703
david.wei <david.wei@nttdata.com>
2017-08-04 14:34:15 +0800

    bootstrap validate

--- src/main/java/com/nezha/controller/UserController.java
+++ src/main/java/com/nezha/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.nezha.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.nezha.entity.User;
 import com.nezha.entity.UserVo;
 import com.nezha.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -38,8 +43,72 @@ public class UserController {
 	}
 
 	@RequestMapping("/registerUser")
-	public String registerUser(User user){
+	public ModelAndView registerUser(User user){
 		userService.registerUser(user);
-		return "index";
+		Map<String,Object> datas = new HashMap<String,Object>();
+		datas.put("registerUser",user);
+		return new ModelAndView("registerSuccess",datas);
+	}
+
+	@RequestMapping("/upload")
+	public String upload(){
+		System.out.println("navigate to upload");
+		return "user/upload";
+	}
+
+	@RequestMapping("fileUpload")
+	public String fileUpload(@RequestParam("fileName")CommonsMultipartFile file){
+		long start = System.currentTimeMillis();
+		String fileName = file.getName();
+		OutputStream os = null;
+		InputStream is = null;
+		try {
+			os = new FileOutputStream("E:/"+new Date().getTime()+file.getOriginalFilename());
+			is = file.getInputStream();
+			int temp ;
+			while ((temp = is.read()) != -1){
+				os.write(temp);
+			}
+			os.flush();
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (os != null){
+				try {
+					os.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (is != null){
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("方法一的运行时间："+String.valueOf(end-start)+"ms");
+		return "icons";
+
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String getUserName(@RequestParam(name = "username",required = true, defaultValue = "david")List<String> username){
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < username.size(); i++) {
+			System.out.println("username = "+username.get(i));
+			sb.append(username.get(i));
+		}
+
+		return sb.toString();
+	}
+
+	@ResponseBody
+	@RequestMapping("/{name}/{id}")
+	public String getUser(@PathVariable("name") String name,@PathVariable("id") int id){
+		return "id="+id+";name="+name;
 	}
 }
